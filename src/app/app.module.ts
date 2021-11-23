a93ba79afc350483c30f6ab907388dce5421fd53
Peter Gakure <petergakure97@gmail.com>
2021-11-23 14:34:59 +0300

    import snotify and ngxspinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NgxSpinnerModule } from 'node_modules/ngx-spinner';
+import { ContactComponent } from './components/contact/contact.component';
+import { SnotifyModule } from 'ng-snotify';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    AboutComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSpinnerModule,
+    SnotifyModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
