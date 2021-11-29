82c7c6f6219487648c17a7bf9f62af4274595d8d
Peter Gakure <petergakure97@gmail.com>
2021-11-29 15:02:31 +0300

    Update modified folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,23 @@ import { FooterComponent } from './components/footer/footer.component';
 import { NgxSpinnerModule } from 'node_modules/ngx-spinner';
 import { ContactComponent } from './components/contact/contact.component';
 import { SnotifyModule } from 'ng-snotify';
+import { HomeComponent } from './components/home/home.component';
+import { EducationComponent } from './components/education/education.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
+import { SkillsComponent } from './components/skills/skills.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent
+  }
+];
+
+const config: ExtraOptions =  {
+  useHash: true,
+}
 
 @NgModule({
   declarations: [
@@ -19,7 +37,11 @@ import { SnotifyModule } from 'ng-snotify';
     AboutComponent,
     HeaderComponent,
     FooterComponent,
-    ContactComponent
+    ContactComponent,
+    HomeComponent,
+    EducationComponent,
+    ProjectsComponent,
+    SkillsComponent
   ],
   imports: [
     BrowserModule,
@@ -27,6 +49,10 @@ import { SnotifyModule } from 'ng-snotify';
     NgxSpinnerModule,
     SnotifyModule,
     FormsModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    // BrowserAnimationsModule,
+    RouterModule.forRoot(routes, config)
   ],
   providers: [],
   bootstrap: [AppComponent]
