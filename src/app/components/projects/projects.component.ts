c7e34052b0100ab3c04441a1f6b3ea789dd27085
Peter Gakure <petergakure97@gmail.com>
2021-11-29 14:58:27 +0300

    Added projects to be displayed

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileService } from 'src/app/services/profile.service';
 
 @Component({
   selector: 'app-projects',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.css']
 })
 export class ProjectsComponent implements OnInit {
+  config: any;
+  projects: any = [];
 
-  constructor() { }
+  constructor(private service: ProfileService) { }
 
   ngOnInit(): void {
+    this.projects = this.service.getProjects()
+    console.log(this.projects)
+    this.config = {
+      itemsPerPage: 3,
+      currentPage: 1,
+      totalItems: this.projects.length
+    };
+  }
+  pageChanged(event: any){
+    this.config.currentPage = event;
   }
 
 }
